2ab750533ec13eaf8d4da7e9326d841d41f29fda
Mohammad Nafis <abidalnafis@gmail.com>
2020-09-22 14:18:46 -0400

    WIP

---
+++ app/src/main/java/com/example/firebase_exercise/IntentLauncher.kt
@@ -0,0 +1,10 @@
+package com.example.firebase_exercise
+//
+//import android.content.Intent
+//import javax.inject.Inject
+//class IntentLauncher @Inject constructor(private val activity: BaseActivity) {
+//
+//    fun launchSignInActivity() {
+//        activity.startActivity(Intent())
+//    }
+//}
--- app/src/main/java/com/example/firebase_exercise/common/FirebaseManager.kt
+++ app/src/main/java/com/example/firebase_exercise/common/FirebaseManager.kt
@@ -12,7 +12,9 @@ import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.database.*
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.annotations.NonNull
 import io.reactivex.rxjava3.core.Completable
+import io.reactivex.rxjava3.core.CompletableEmitter
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.schedulers.Schedulers
 import javax.inject.Inject
@@ -26,89 +28,119 @@ class FirebaseManager @Inject constructor(
     fun getCurrentUser(): FirebaseUser? = firebaseAuth.currentUser
 
     fun getMovieDataObservable(): Observable<List<Movie>> =
-        Observable.create<DataSnapshot> {
-            databaseReference.child(firebaseAuth.currentUser!!.email!!.getUserChild())
-                .addValueEventListener(object : ValueEventListener {
-                    override fun onCancelled(error: DatabaseError) {}
-
-                    override fun onDataChange(snapshot: DataSnapshot) {
-                        it.onNext(snapshot)
-                    }
-                })
-        }.map {
-            if (it.exists()) {
-                val t: GenericTypeIndicator<Map<String, Movie>> =
-                    object : GenericTypeIndicator<Map<String, Movie>>() {}
-                val map = it.getValue(t) as Map<String, Movie>
-                map.values.toList()
-            } else {
-                listOf()
-            }
-        }.addSchedulers()
-
-    fun addMovie(newMovie: Movie): Completable = Completable.create { emitter ->
-        databaseReference.child(firebaseAuth.currentUser!!.email!!.getUserChild())
-            .addListenerForSingleValueEvent(object : ValueEventListener {
+        Observable.create<DataSnapshot> { emitter ->
+            object : ValueEventListener {
                 override fun onCancelled(error: DatabaseError) {
-                    emitter.onError(error.toException())
+                    emitter.onComplete()
                 }
 
                 override fun onDataChange(snapshot: DataSnapshot) {
-                    if (snapshot.exists()) {
-                        val t: GenericTypeIndicator<Map<String, Movie>> =
-                            object : GenericTypeIndicator<Map<String, Movie>>() {}
-                        val map = snapshot.getValue(t) as Map<String, Movie>
-                        for (movie in map.values) {
-                            if (movie.title == newMovie.title && movie.year == newMovie.year) {
-                                emitter.onError(MovieExistenceException())
-                                return
-                            }
+                    emitter.onNext(snapshot)
+                }
+            }.apply {
+                firebaseAuth.currentUser?.let { currentUser ->
+                    currentUser.email?.let { userEmail ->
+                        databaseReference.child(userEmail.getUserChild())
+                            .addValueEventListener(this)
+                        emitter.setCancellable {
+                            databaseReference.child(userEmail.getUserChild())
+                                .removeEventListener(this)
                         }
                     }
-                    databaseReference.child(firebaseAuth.currentUser!!.email!!.getUserChild())
-                        .push().setValue(newMovie)
-                        .addOnSuccessListener {
-                            emitter.onComplete()
-                        }
-                        .addOnFailureListener {
-                            it.printStackTrace()
-                            emitter.onError(MovieAddingFailedException())
-                        }
                 }
-            })
+            }
+        }.map { getMovieList(it) }.addSchedulers()
+
+    fun addMovie(newMovie: Movie): Completable = Completable.create { emitter ->
+        firebaseAuth.currentUser?.let { currentUser ->
+            currentUser.email?.let { userEmail ->
+                databaseReference.child(userEmail.getUserChild())
+                    .addListenerForSingleValueEvent(object : ValueEventListener {
+                        override fun onCancelled(error: DatabaseError) {
+                            emitter.onError(error.toException())
+                        }
+
+                        override fun onDataChange(snapshot: DataSnapshot) {
+                            if (movieExistsInDatabase(snapshot, newMovie)) {
+                                emitter.onError(MovieExistenceException())
+                            } else {
+                                insertNewMovie(userEmail, newMovie, emitter)
+                            }
+                        }
+                    })
+            }
+        }
     }.addSchedulers()
+
+    private fun insertNewMovie(
+        userEmail: String,
+        newMovie: Movie,
+        emitter: @NonNull CompletableEmitter
+    ) {
+        databaseReference.child(userEmail.getUserChild())
+            .push().setValue(newMovie)
+            .addOnSuccessListener {
+                emitter.onComplete()
+            }
+            .addOnFailureListener {
+                it.printStackTrace()
+                emitter.onError(MovieAddingFailedException())
+            }
+    }
+
+    private fun movieExistsInDatabase(snapshot: DataSnapshot, newMovie: Movie): Boolean {
+        for (movie in getMovieList(snapshot)) {
+            if (movie.title.toLowerCase() == newMovie.title.toLowerCase()
+                && movie.year.toLowerCase() == newMovie.year.toLowerCase()
+            ) return true
+        }
+        return false
+    }
 
     fun signInToFirebaseWithGoogle(data: Intent): Completable =
         Completable.create { emitter ->
             try {
-                val account: GoogleSignInAccount = GoogleSignIn.getSignedInAccountFromIntent(data)
-                    .getResult(ApiException::class.java)!!
-                val credential = GoogleAuthProvider.getCredential(account.idToken, null)
-                firebaseAuth.signInWithCredential(credential).addOnCompleteListener { signInTask ->
-                    if (signInTask.isSuccessful) {
-                        emitter.onComplete()
-                    } else {
-                        emitter.onError(UserSignInFailedException())
+                val account: GoogleSignInAccount? = GoogleSignIn.getSignedInAccountFromIntent(data)
+                    .getResult(ApiException::class.java)
+                account?.let {
+                    val credential = GoogleAuthProvider.getCredential(it.idToken, null)
+                    firebaseAuth.signInWithCredential(credential).addOnCompleteListener { signIn ->
+                        if (signIn.isSuccessful) {
+                            emitter.onComplete()
+                        } else {
+                            emitter.onError(UserSignInFailedException())
+                        }
                     }
                 }
-            } catch (e: ApiException) {
+            } catch (e: Exception) {
                 emitter.onError(UserSignInFailedException())
             }
         }.addSchedulers()
 
-    fun userSignOut(): Completable =
-        Completable.create { emitter ->
-            firebaseAuth.signOut()
-            googleSignInClient.signOut().addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    emitter.onComplete()
-                } else {
-                    emitter.onError(UserSignOutFailedException())
-                }
+    fun userSignOut(): Completable = Completable.create { emitter ->
+        firebaseAuth.signOut()
+        googleSignInClient.signOut().addOnCompleteListener { signOut ->
+            if (signOut.isSuccessful) {
+                emitter.onComplete()
+            } else {
+                emitter.onError(UserSignOutFailedException())
             }
-        }.addSchedulers()
+        }
+    }.addSchedulers()
+
+    private fun getMovieList(snapshot: DataSnapshot): List<Movie> {
+        return if (snapshot.exists()) {
+            val t: GenericTypeIndicator<Map<String, Movie>> =
+                object : GenericTypeIndicator<Map<String, Movie>>() {}
+            val map = snapshot.getValue(t) as Map<String, Movie>
+            map.values.toList()
+        } else {
+            listOf()
+        }
+    }
 
     private fun String.getUserChild() = this.replace(Regex("[.#$\\[\\]]"), "")
+
     private fun Completable.addSchedulers(): Completable =
         this.subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
 
